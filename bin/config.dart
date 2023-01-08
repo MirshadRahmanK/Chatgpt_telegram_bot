e50ebe8093c341b03557f8365eb267425efb540e
Mirshad Kvr <98144132+MirshadRahmanK@users.noreply.github.com>
2023-01-08 13:07:44 +0530

    Update config.dart

--- bin/config.dart
+++ bin/config.dart
@@ -1,2 +1,2 @@
-String bot_toCken = '5681916697:AAFtT0gosvbXn0KCoCUvO3RBvIFOlp1bgyg';
-String chat_gpt_api = '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';
+String bot_toCken = '';
+String chat_gpt_api = '';
